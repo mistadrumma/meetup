68848bde31ea6e7cdf2ac9c572ace741d4ad4708
Mistadrumma <breakroger104@gmail.com>
2017-10-29 21:54:33 +0300

    creat repo

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+		<v-navigation-drawer
+			temporary
+			v-model="sideNav"
+		>
+			<v-list dense class="pt-0">
+				<v-list-tile  v-for="item in menuItem" :key="item.title" router :to="item.link">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+			</v-list>
+	</v-navigation-drawer>
+		  <v-toolbar class="teal" dark>
+				<v-toolbar-side-icon temporary @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+		    	<v-toolbar-title>
+					<router-link to="/" tag='span' style="cursor: pointer">{{ label }}</router-link>
+					</v-toolbar-title>
+		    <v-spacer></v-spacer>
+		    <v-toolbar-items class="hidden-sm-and-down" v-for="item in menuItem" :key="item.title">
+		      <v-btn flat router :to="item.link">
+						<v-icon left>{{ item.icon }}</v-icon>
+		        {{ item.title }}
+		      </v-btn>
+		    </v-toolbar-items>
+		  </v-toolbar>
+    <main>
+			<router-view></router-view>
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+				label: 'Meet Up',
+        sideNav: false,
+          menuItem: [
+					{ icon: 'supervisor_account',title: 'View Meetups', link: '/meetups'},
+					{ icon: 'room', title: 'Organize Meetups', link: '/meetups/new'},
+					{ icon: 'person', title: 'Profile', link: '/profile'},
+					{ icon: 'face', title: 'Sign up', link: '/signup'},
+					{ icon: 'lock_open', title: 'Sign in', link: '/signin'}
+				]
+      }
+    }
+  }
+
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import home from '@/components/home'
+import Meetups from '@/components/meetup/Meetups'
+import CreateMeetup from '@/components/meetup/CreateMeetup'
+import Profile from '@/components/user/Profile'
+import SignIn from '@/components/user/SignIn'
+import SignUp from '@/components/user/SignUp'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/meetups',
+      name: 'Meetups',
+      component: Meetups
+    },
+    {
+      path: '/meetups/new',
+      name: 'CreateMeetup',
+      component: CreateMeetup
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
+    },
+    {
+      path: '/signin',
+      name: 'SignIn',
+      component: SignIn
+    },
+  ],
+  mode: 'history'
+})
