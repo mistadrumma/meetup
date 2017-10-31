0879032fb84d5df85934450fef20392e213cf1ea
Mistadrumma <breakroger104@gmail.com>
2017-10-31 22:10:30 +0300

    16

--- src/components/meetup/meetup.vue
+++ src/components/meetup/meetup.vue
@@ -4,9 +4,27 @@
 		<v-flex xs12>
 			<v-card>
 				<v-card-title>
-					My Meetup
+					<h6 class="accent--text">My Meetup</h6>
 				</v-card-title>
-			</v-card>	
+					<v-card-media
+								src="https://static.pexels.com/photos/587839/pexels-photo-587839.jpeg"
+								height="400px"
+				></v-card-media>
+				<v-card-text>
+					<div class="info--text">
+
+						17th Jule 2017 - Where it takes placekes place
+					</div>
+					<div class="">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+						 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+							\ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+							sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</v-card-text>
+			</v-card>
 		</v-flex>
 	</v-layout>
 </v-container>
